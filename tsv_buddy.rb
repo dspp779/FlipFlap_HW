a7887d7367afcbb9d07a6f9771776e44ce76f54e
JJC <jjc@nlplab.cc>
2016-09-30 18:33:00 +0800

    tsv and yaml buddy!

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -1,14 +1,18 @@
+require 'csv'
 # Module that can be included (mixin) to take and output TSV data
 module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
-
+    @data = CSV.parse(tsv, col_sep: "\t", headers: true).map(&:to_h)
   end
 
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
   def to_tsv
-
+    CSV.generate(col_sep: "\t") do |tsv|
+      tsv << data.first.keys
+      data.to_a.each { |item| tsv << item.values }
+    end
   end
 end
